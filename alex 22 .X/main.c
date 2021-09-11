1a1b287f58be8138183b0a23bacb2a2aaee7f4eb
AmrAlamoudi <amr.alshair15@gmail.com>
2021-09-11 16:34:11 +0200

    new project

---
+++ alex 22 .X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: LENOVO
+ *
+ * Created on September 11, 2021, 4:18 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
